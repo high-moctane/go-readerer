191cf9d5c8c221865398435246da24445b8589ac
high-moctane <high.moctane@gmail.com>
2019-12-31 00:04:01 +0900

    change test from ioutil.ReadAll to bufio.Scanner

--- io_test.go
+++ io_test.go
@@ -1,8 +1,7 @@
 package readerer
 
 import (
-	"io"
-	"io/ioutil"
+	"bufio"
 	"os"
 	"testing"
 )
@@ -10,23 +9,25 @@ import (
 func TestFromReaderAt(t *testing.T) {
 	tests := []struct {
 		offset int64
-		str    string
+		lines  []string
 		err    error
 	}{
-		{0, `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-culpa qui officia deserunt mollit anim id est laborum.
-`,
+		{
+			0,
+			[]string{"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
+				"incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis",
+				"nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+				"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu",
+				"fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in",
+				"culpa qui officia deserunt mollit anim id est laborum."},
 			nil,
 		},
-		{335, `Excepteur sint occaecat cupidatat non proident, sunt in
-culpa qui officia deserunt mollit anim id est laborum.
-`,
-			nil},
-		{446, "", io.EOF},
+		{
+			335,
+			[]string{"Excepteur sint occaecat cupidatat non proident, sunt in",
+				"culpa qui officia deserunt mollit anim id est laborum."},
+			nil,
+		},
 	}
 
 	f, err := os.Open("testdata/lorem_ipsum.txt")
@@ -36,12 +37,14 @@ culpa qui officia deserunt mollit anim id est laborum.
 
 	for idx, test := range tests {
 		r := FromReaderAt(f, test.offset)
-		b, err := ioutil.ReadAll(r)
-		if err != nil {
-			t.Fatalf("[%d] unexpected error", idx)
+		sc := bufio.NewScanner(r)
+		for i := 0; sc.Scan(); i++ {
+			if test.lines[i] != sc.Text() {
+				t.Errorf("[%d] expected %s, but got %s", idx, test.lines[i], sc.Text())
+			}
 		}
-		if test.str != string(b) {
-			t.Errorf("[%d] expected %s, got %s", idx, test.str, string(b))
+		if test.err != sc.Err() {
+			t.Errorf("[%d] expected %v, but got %v", idx, test.err, sc.Err())
 		}
 	}
 }
